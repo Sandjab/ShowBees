238b91fbe4eb9a141b26709acfee5e453271c885
Jean-Paul Gavini <46782050+Sandjab@users.noreply.github.com>
2023-04-27 14:06:44 +0200

    Update for compatibility with pyprojroot 0.3.0

--- experiments/EXP01 - BEE-NOBEE CLASSIFIERS/kilroy_was_here.py
+++ experiments/EXP01 - BEE-NOBEE CLASSIFIERS/kilroy_was_here.py
@@ -1,9 +1,12 @@
 """ Little helper allowing import of modules from the codelib directory and build of absolute path from project root, based on a marker file"""
 import sys
-from pyprojroot import here
+import pyprojroot
 from os import fspath
-from os.path import join
+
+base_path = pyprojroot.find_root(pyprojroot.has_file(".kilroy"))
+
 try:
-    sys.path.append(fspath(here(project_files=['.kilroy'], warn=True)))
+    #sys.path.append(fspath(here(has_file=".kilroy", warn=True)))
+    sys.path.append(fspath(base_path))
 except (RecursionError):
     raise FileNotFoundError('Marker file (.kilroy) not found in project tree')
